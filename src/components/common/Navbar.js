8eb75d1e4b7f064db5d398068d9d721bef1d668d
Amir Sohi <amirasohi@gmail.com>
2018-04-06 13:43:06 +0100

    Created logout route

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -12,6 +12,11 @@ class Navbar extends React.Component{
     this.setState({ navIsOpen: !this.state.navIsOpen });
   }
 
+  handleLogout = () => {
+    Auth.logout();
+    this.props.history.push('/');
+  }
+
   componentWillUpdate(){
     this.state.navIsOpen && this.setState({ navIsOpen: false });
   }
@@ -36,8 +41,8 @@ class Navbar extends React.Component{
         <div className={`navbar-menu ${this.state.navIsOpen ? 'is-active' : ''}`}>
           <div className="navbar-end">
             <Link className="navbar-item" to="/days">Calender</Link>
-            {/* {Auth.isAuthenticated() && <Link className="navbar-item" to="/bangers/new">New Banger</Link>}
-            {Auth.isAuthenticated() && <a className="navbar-item" onClick={this.handleLogout}>Logout</a>} */}
+            {/* {Auth.isAuthenticated() && <Link className="navbar-item" to="/bangers/new">New Banger</Link>} */}
+            {Auth.isAuthenticated() && <a className="navbar-item" onClick={this.handleLogout}>Logout</a>}
             {!Auth.isAuthenticated() && <Link className="navbar-item" to="/login">Login</Link>}
             {!Auth.isAuthenticated() && <Link className="navbar-item" to="/register">Register</Link>}
           </div>
