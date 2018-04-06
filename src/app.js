11cc30304ba2db4b95fb5f6c84caebed4add218c
Amir Sohi <amirasohi@gmail.com>
2018-04-06 11:33:57 +0100

    Created Navbar and Auth component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
+
+import 'bulma';
+
+import Navbar from './components/common/Navbar';
+
 class App extends React.Component {
   render() {
     return (
-      <h1>WDI32 Project 4</h1>
+      <BrowserRouter>
+        <main>
+          <Navbar />
+          <section className="section">
+
+          </section>
+        </main>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/common/Navbar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { Link, withRouter} from 'react-router-dom';
+
+class Navbar extends React.Component{
+  state = {
+    navIsOpen: false
+  }
+
+  handleToggle = () => {
+    this.setState({ navIsOpen: !this.state.navIsOpen });
+  }
+
+  componentWillUpdate(){
+    this.state.navIsOpen && this.setState({ navIsOpen: false });
+  }
+
+  render(){
+    return(
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <Link className="navbar-item" to="/">
+          Shwap
+          </Link>
+          {/* if the navbarIsOpen is true the navBar is active otherwise it's falsey */}
+          <div
+            onClick={this.handleToggle}
+            className={`navbar-burger ${this.state.navIsOpen ? 'is-active' : ''}`}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div className={`navbar-menu ${this.state.navIsOpen ? 'is-active' : ''}`}>
+          <div className="navbar-end">
+            <Link className="navbar-item" to="/days">Calender</Link>
+            {/* {Auth.isAuthenticated() && <Link className="navbar-item" to="/bangers/new">New Banger</Link>}
+            {Auth.isAuthenticated() && <a className="navbar-item" onClick={this.handleLogout}>Logout</a>}
+            {!Auth.isAuthenticated() && <Link className="navbar-item" to="/login">Login</Link>}
+            {!Auth.isAuthenticated() && <Link className="navbar-item" to="/register">Register</Link>} */}
+          </div>
+        </div>
+      </nav>
+    );
+  }
+
+}
+
+export default withRouter(Navbar);
